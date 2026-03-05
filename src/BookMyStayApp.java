5161405a8f3ac0c8b5eeac5513a47444c1b97bd0
vignesh-reddy-c <ga6626192@gmail.com>
2026-03-05 15:24:36 +0530

    Use Case 3: Centralized Room Inventory Management

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,58 +1,34 @@
-abstract class Room {
-    String roomType;
-    int beds;
-    double price;
-
-    Room(String roomType, int beds, double price) {
-        this.roomType = roomType;
-        this.beds = beds;
-        this.price = price;
+import java.util.HashMap;
+import java.util.Map;
+class RoomInventory {
+    private HashMap<String, Integer> inventory;
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 10);
+        inventory.put("Double Room", 5);
+        inventory.put("Suite Room", 2);
     }
-
-    void displayDetails() {
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price per Night: " + price);
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
     }
-}
-
-class SingleRoom extends Room {
-    SingleRoom() {
-        super("Single Room", 1, 2000);
+    public void updateAvailability(String roomType, int count) {
+        inventory.put(roomType, count);
     }
-}
-
-class DoubleRoom extends Room {
-    DoubleRoom() {
-        super("Double Room", 2, 3500);
+    public void displayInventory() {
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " Available: " + entry.getValue());
+        }
     }
 }
-
-class SuiteRoom extends Room {
-    SuiteRoom() {
-        super("Suite Room", 3, 6000);
-    }
-}
-
 public class BookMyStayApp{
     public static void main(String[] args) {
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
-
-        int singleAvailable = 10;
-        int doubleAvailable = 5;
-        int suiteAvailable = 2;
-
-        System.out.println("Book My Stay - Hotel Booking System v2.1\n");
-
-        single.displayDetails();
-        System.out.println("Available: " + singleAvailable + "\n");
-
-        doubleRoom.displayDetails();
-        System.out.println("Available: " + doubleAvailable + "\n");
-
-        suite.displayDetails();
-        System.out.println("Available: " + suiteAvailable);
+        System.out.println("Book My Stay - Hotel Booking System v3.1\n");
+        RoomInventory inventory = new RoomInventory();
+        System.out.println("Current Room Inventory:");
+        inventory.displayInventory();
+        System.out.println("\nUpdating Single Room availability...");
+        inventory.updateAvailability("Single Room", 8);
+        System.out.println("\nUpdated Room Inventory:");
+        inventory.displayInventory();
     }
 }
